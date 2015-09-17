7101870475efb69095b5e151f0633dbb13d63e30
Sascha <beck@wak-online.de>
2015-09-17 09:57:51 +0200

    Gulpü

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,27 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var uglyfly = require('gulp-uglyfly');
 
-gulp.task('sass', function(){
+gulp.task('sass', function () {
 
-return gulp.src('res/sass/main.scss')
-.pipe(sass())
-.pipe(gulp.dest('res/css'))
+    return gulp.src('res/sass/style.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('res/css'))
+
+});
+
+//watching scss
+gulp.task('watch', function () {
+
+    gulp.watch('res/**/*.scss', ['sass']);
 
 });
 
 
-gulp.task('default', ['sass']);
+gulp.task('compress', function () {
+    gulp.src('res/js/*.js')
+        .pipe(uglyfly())
+        .pipe(gulp.dest('res/dist'))
+});
+
+gulp.task('default', ['sass', 'compress', 'watch']);
